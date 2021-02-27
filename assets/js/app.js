bce0e1c56bb30df9407a93e68b94731d2b363696
Fabiano Basso <fabiano.basso.dev@gmail.com>
2021-02-26 21:11:41 -0300

    Portfolio github - Fabiano Basso

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,4 +5,28 @@ $(document).ready(() =>{
         $('.social-link').fadeToggle(1000)
     })
 
+    $('#resumo').on('click', e =>{
+        $(e.target).addClass('active')
+        removeActive(['#projetos', '#contato'])
+        
+    })
+
+    $('#projetos').on('click', e =>{
+        $(e.target).addClass('active')
+        removeActive(['#resumo', '#contato'])
+    })
+
+    $('#contato').on('click', e =>{
+        $(e.target).addClass('active')
+        removeActive(['#projetos', '#resumo'])
+    })
+
+    //Fuction remove class active
+    function removeActive(classes = [], className = 'active'){
+        for(i = 0; i < 2; i++){
+            if($(classes[i]).hasClass(className)){
+                $(classes[i]).removeClass(className)
+            }
+        }
+    }
 })
